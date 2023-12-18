0d7cdbb7fea61b1839ddf5151c2ab984b3364e21
Yilin Ye <66715314+Yilinista@users.noreply.github.com>
2023-12-17 19:15:55 -0500

    New version

--- model.py
+++ model.py
@@ -1,21 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
 
 def ResNetSimCLR(input_dim, output_dim):
     # Input layer
     inputs = Input(shape=input_dim)
 
     # Base Encoder: ResNet50 without the top layer and pretrained weights
-    base_encoder = ResNet50(include_top=False, weights=None, pooling='avg', input_tensor=inputs)
+    base_encoder = ResNet50(include_top=False, weights='imagenet', pooling='avg', input_tensor=inputs)
+    base_encoder.trainable = True
     
     # Pass inputs through the encoder
-    h = base_encoder(inputs)
+    h = base_encoder(inputs,training=True)
 
-    # Projection Head: Two dense layers on top of the base encoder
-    x = Dense(units=output_dim, activation='relu')(h)  # First layer with ReLU activation
-    x = Dense(units=output_dim)(x)  # Second layer
+    # Projection Head: Three dense layers on top of the base encoder
+    x = Dense(units=256, activation='relu')(h)
+    x = Dense(units=128, activation='relu')(h)
+    x = Dense(units=output_dim)(x)
 
     # Model outputs: raw features and projection head output
     return Model(inputs=inputs, outputs=[h, x])
